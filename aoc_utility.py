26ec4e7f70939225a2d6ae4fc4ca51a7ebc05e9d
DataKumpel <86472041+DataKumpel@users.noreply.github.com>
2022-12-03 14:43:01 +0100

    Added utility for getting puzzle input

---
+++ aoc_utility.py
@@ -0,0 +1,11 @@
+
+
+def get_puzzle_input(number: int) -> str:
+    with open(f"input_{number:02}.txt") as input_file:
+        return input_file.read()
+
+
+if __name__ == "__main__":
+    # Testing first 3 puzzle inputs:
+    for i in range(1, 4):
+        print(get_puzzle_input(i))
