3db1290180338adf391a9e6721241378f71f7077
DataKumpel <86472041+DataKumpel@users.noreply.github.com>
2022-12-04 13:10:33 +0100

    Doc string added

--- aoc_utility.py
+++ aoc_utility.py
@@ -1,6 +1,19 @@
 
 
 def get_puzzle_input(number: int) -> str:
+    """Load the input of a inputfile on the same level as cwd.
+
+    Parameters
+    ----------
+    number : int
+        number of the input file => input_02.txt, input_12.txt, etc.
+
+    Returns
+    -------
+    str
+        The input of the given puzzle.
+    """
+    
     with open(f"input_{number:02}.txt") as input_file:
         return input_file.read()
 
